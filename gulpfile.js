7d2f887fcd617d3be7b2d7ad61da289ffc447cac
Michele Peghini <michelepeghini@gmail.com>
2018-01-24 14:55:08 +1100

    html copy gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,45 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp = require('gulp'),
+sass = require('gulp-sass'),
+browserSync = require('browser-sync'),
+autoprefixer = require('gulp-autoprefixer');
+
+const SOURCEPATHS = {
+  sassSource: 'src/scss/*.scss',
+  htmlSource: 'src/*.html'
+};
+
+const APPAPTH = {
+  root: 'app/',
+  css: 'app/css',
+  js: 'app/js'
+};
+
+gulp.task('serve', ['sass'], function() {
+  browserSync.init([APPAPTH.css + '/*.css', APPAPTH.root + '/*.html', APPAPTH.js + '/*.js'], {
+    server: {
+      baseDir: APPAPTH.root
+    }
+  });
+});
 
 gulp.task('sass', function(){
-  return gulp.src('src/scss/app.scss')
+  return gulp.src(SOURCEPATHS.sassSource)
+    .pipe(autoprefixer())
     .pipe(sass({
       outputStyle: 'expanded'
     }).on('error', sass.logError))
-    .pipe(gulp.dest('app/css'))
+    .pipe(gulp.dest(APPAPTH.css));
 });
 
-gulp.task('default', ['sass']);
+gulp.task('copy', function() {
+    return gulp.src(SOURCEPATHS.htmlSource)
+      .pipe(gulp.dest(APPAPTH.root));
+});
+
+gulp.task('watch', ['serve', 'sass', 'copy'], function() {
+  gulp.watch([SOURCEPATHS.sassSource], ['sass']);
+  gulp.watch([SOURCEPATHS.htmlSource], ['copy']);
+
+});
+
+gulp.task('default', ['watch']);
