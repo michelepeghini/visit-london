5d2273f139a5af37313a67861cb2fdffe2c356a0
Michele Peghini <michelepeghini@gmail.com>
2018-01-24 15:33:24 +1100

    clean js gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 var gulp = require('gulp'),
 sass = require('gulp-sass'),
 browserSync = require('browser-sync'),
-autoprefixer = require('gulp-autoprefixer');
+autoprefixer = require('gulp-autoprefixer'),
+clean = require('gulp-clean');
 
 const SOURCEPATHS = {
   sassSource: 'src/scss/*.scss',
-  htmlSource: 'src/*.html'
+  htmlSource: 'src/*.html',
+  jsSource: 'src/js/*.js',
 };
 
 const APPAPTH = {
@@ -13,6 +15,16 @@ const APPAPTH = {
   css: 'app/css',
   js: 'app/js'
 };
+
+gulp.task('scripts', ['clean-js'], function() {
+    return gulp.src(SOURCEPATHS.jsSource)
+      .pipe(gulp.dest(APPAPTH.js));
+});
+
+gulp.task('clean-js', function() {
+  return gulp.src(APPAPTH.js + '/*.js', {read: false, force: true})
+    .pipe(clean());
+});
 
 gulp.task('serve', ['sass'], function() {
   browserSync.init([APPAPTH.css + '/*.css', APPAPTH.root + '/*.html', APPAPTH.js + '/*.js'], {
@@ -31,14 +43,20 @@ gulp.task('sass', function(){
     .pipe(gulp.dest(APPAPTH.css));
 });
 
-gulp.task('copy', function() {
+gulp.task('copy', ['clean-html'], function() {
     return gulp.src(SOURCEPATHS.htmlSource)
       .pipe(gulp.dest(APPAPTH.root));
 });
 
-gulp.task('watch', ['serve', 'sass', 'copy'], function() {
+gulp.task('clean-html', function() {
+  return gulp.src(APPAPTH.root + '/*.html', {read: false, force: true})
+    .pipe(clean());
+});
+
+gulp.task('watch', ['serve', 'sass', 'copy', 'scripts'], function() {
   gulp.watch([SOURCEPATHS.sassSource], ['sass']);
   gulp.watch([SOURCEPATHS.htmlSource], ['copy']);
+  gulp.watch([SOURCEPATHS.jsSource], ['scripts']);
 
 });
 
